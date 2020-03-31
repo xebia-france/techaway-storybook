62aa0aa1bcd828d331fa2db60da840ce9b8faded
Jennifer Proust <jennifer.proust@gmail.com>
2020-03-31 20:23:21 +0200

    feat(storybook i18n): parse locales dynamically

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -2,9 +2,20 @@ import { withI18next } from "storybook-addon-i18next";
 import { addDecorator } from "@storybook/react";
 import i18n from "i18n";
 
+const languagesNamesMap = { fr: "French", en: "English" };
+
+const languages = require
+  .context("../public/locales", true, /\.json$/)
+  .keys()
+  .map((filename) => filename.substr(2).replace(/\.[^/.]+$/, ""))
+  .reduce((languages, key) => {
+    languages[key] = languagesNamesMap[key] || key;
+    return languages;
+  }, {});
+
 addDecorator(
   withI18next({
     i18n,
-    languages: { fr: "French", en: "English" },
+    languages,
   })
 );
